070e741843f17ba17b5787980d6695fd0beaadb5
RoboRace <roborace@sirious.nl>
2025-09-29 13:30:26 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 IR.IR_callbackUser(function () {
-    if (IR.IR_read() == 24) {
+    if (IR.IR_read() == 157) {
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -8,7 +8,7 @@ IR.IR_callbackUser(function () {
             . . # . .
             `)
     }
-    if (IR.IR_read() == 8) {
+    if (IR.IR_read() == 221) {
         basic.showLeds(`
             . . # . .
             . # # . .
@@ -17,7 +17,7 @@ IR.IR_callbackUser(function () {
             . . # . .
             `)
     }
-    if (IR.IR_read() == 165) {
+    if (IR.IR_read() == 61) {
         basic.showLeds(`
             . . # . .
             . . # # .
@@ -26,7 +26,7 @@ IR.IR_callbackUser(function () {
             . . # . .
             `)
     }
-    if (IR.IR_read() == 181) {
+    if (IR.IR_read() == 21) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -35,7 +35,7 @@ IR.IR_callbackUser(function () {
             . . # . .
             `)
     }
-    if (IR.IR_read() == 199) {
+    if (IR.IR_read() == 253) {
         basic.showLeds(`
             . . . . #
             . . . # .
@@ -44,52 +44,52 @@ IR.IR_callbackUser(function () {
             . . . . .
             `)
     }
+    if (IR.IR_read() == 189) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    if (IR.IR_read() == 173) {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            . # . # .
+            # # # # #
+            . # . # .
+            `)
+    }
     if (IR.IR_read() == 22) {
-        basic.showLeds(`
-            # . # . #
-            . # # # .
-            # # # # #
-            . # # # .
-            # . # . #
-            `)
-    }
-    if (IR.IR_read() == 13) {
-        basic.showLeds(`
-            . # . # .
-            # # # # #
-            . # . # .
-            # # # # #
-            . # . # .
-            `)
-    }
-    if (IR.IR_read() == 93) {
         basic.showNumber(1)
-        if (IR.IR_read() == 157) {
-            basic.showNumber(2)
-        }
-    }
-    if (IR.IR_read() == 29) {
-        basic.showNumber(3)
-    }
-    if (IR.IR_read() == 221) {
-        basic.showNumber(4)
-    }
-    if (IR.IR_read() == 253) {
-        basic.showNumber(5)
-    }
-    if (IR.IR_read() == 61) {
-        basic.showNumber(6)
-    }
-    if (IR.IR_read() == 31) {
-        basic.showNumber(7)
-    }
-    if (IR.IR_read() == 21) {
-        basic.showNumber(8)
-    }
-    if (IR.IR_read() == 9) {
-        basic.showNumber(9)
     }
     if (IR.IR_read() == 25) {
+        basic.showNumber(2)
+    }
+    if (IR.IR_read() == 13) {
+        basic.showNumber(3)
+    }
+    if (IR.IR_read() == 12) {
+        basic.showNumber(4)
+    }
+    if (IR.IR_read() == 24) {
+        basic.showNumber(5)
+    }
+    if (IR.IR_read() == 133) {
+        basic.showNumber(6)
+    }
+    if (IR.IR_read() == 8) {
+        basic.showNumber(7)
+    }
+    if (IR.IR_read() == 199) {
+        basic.showNumber(8)
+    }
+    if (IR.IR_read() == 165) {
+        basic.showNumber(9)
+    }
+    if (IR.IR_read() == 181) {
         basic.showNumber(0)
     }
 })
